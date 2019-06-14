63cf8d92c3a35da58f5c80d303f724ae6214de20
FFDevGuru <playzadev@gmail.com>
2019-06-14 20:43:03 +0100

    First Upoad

---
+++ demo/assets/beackpanorama/beackpanorama_actions.js
@@ -0,0 +1,10 @@
+var mc_symbol_Symbol_202_89_0_fn = function() {
+
+/* Stop at This Frame
+The  timeline will stop/pause at the frame where you insert this code.
+Can also be used to stop/pause the timeline of movieclips.
+*/
+
+this.stop();
+
+};
